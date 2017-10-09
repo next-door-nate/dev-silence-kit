b95b788e02cf299f830820c77f793d6a34d478ae
Nate <natevandervis@gmail.com>
2017-10-09 13:21:54 -0500

    moving junk to src / adding social links / custo analytics tracking

--- footer.php
+++ footer.php
@@ -1,42 +1,43 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+<!-- footer -->
+<footer class="footer" role="contentinfo">
 
-				<section class="social-media">
-					<nav>
-						<a class="social-media__facebook" href="https://facebook.com"><i class="fa fa-facebook"></i></a>
-						<a class="social-media__twitter" href="https://twitter.com"><i class="fa fa-twitter"></i></a>
-						<a class="social-media__instagram" href="https://instagram.com"><i class="fa fa-instagram"></i></a>
-						<a class="social-media__spotify" href="https://spotify.com"><i class="fa fa-spotify"></i></a>
-						<a class="social-media__apple" href="https://apple.com"><i class="fa fa-apple"></i></a>
-						<a class="social-media__google" href="https://google.com"><i class="fa fa-play"></i></a>
-					</nav>
-				</section>
+	<section class="social-media">
+		<nav>
+			<a class="social-media__facebook" href="https://www.facebook.com/silencekitband/"><i class="fa fa-facebook"></i></a>
+			<a class="social-media__twitter" href="https://twitter.com/silencekitwpg"><i class="fa fa-twitter"></i></a>
+			<a class="social-media__youtube" href="https://www.youtube.com/channel/UCKwjkBvNG-py7GCvBjv-9ZA"><i class="fa fa-youtube-play"></i></a>
+			<a class="social-media__instagram" href="https://www.instagram.com/silencekitband/"><i class="fa fa-instagram"></i></a>
+			<a class="social-media__spotify" href="https://open.spotify.com/artist/6NOcxPjWXYJxR5jnfFAWkC"><i class="fa fa-spotify"></i></a>
+			<a class="social-media__apple" href="https://itunes.apple.com/ca/artist/silence-kit/id1236258633"><i class="fa fa-apple"></i></a>
+			<a class="social-media__google" href="https://play.google.com/store/music/album/Silence_Kit_Started_as_a_Whisper?id=Blopx6di5wuf4lb67q6wcdjv6fq"><i class="fa fa-play"></i></a>
+		</nav>
+	</section>
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> - <?php bloginfo('name'); ?> - All rights reserved -
-					Site by <a href="//slowjams.co" title="A Slowjams Joint">Slow Jams</a>.
-				</p>
-				<!-- /copyright -->
+	<!-- copyright -->
+	<p class="copyright">
+		&copy; <?php echo date('Y'); ?> - <?php bloginfo('name'); ?> - All rights reserved -
+		Site by <a href="//slowjams.co" title="A Slowjams Joint">Slow Jams</a>.
+	</p>
+	<!-- /copyright -->
 
-			</footer>
-			<!-- /footer -->
+</footer>
+<!-- /footer -->
 
-		</div>
-		<!-- /wrapper -->
+</div>
+<!-- /wrapper -->
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
-		<!-- super fresh analytics -->
-		<script>
-			!function(t,r,k,S,T,A,R){t.GoogleAnalyticsObject=k;t[k]||(t[k]=function(){
-			(t[k].q=t[k].q||[]).push(arguments)});t[k].l=+new Date;A=r.createElement(S);
-			R=r.getElementsByTagName(S)[0];A.src=T;R.parentNode.insertBefore(A,R)}
-			(window,document,'ga','script','//www.google-analytics.com/analytics.js');
+<!-- super fresh analytics -->
+<script>
+!function(s,i,l,e,n,c,E){s.GoogleAnalyticsObject=l;s[l]||(s[l]=function(){
+(s[l].q=s[l].q||[]).push(arguments)});s[l].l=+new Date;c=i.createElement(e);
+E=i.getElementsByTagName(e)[0];c.src=n;E.parentNode.insertBefore(c,E)}
+(window,document,'ga','script','//www.google-analytics.com/analytics.js');
 
-			ga('create', 'UA-29923838-10', 'auto');
-			ga('send', 'pageview');
-		</script>
+ga('create', 'UA-29923838-10', 'auto');
+ga('send', 'pageview');
+</script>
 
-	</body>
+</body>
 </html>
--- src/templates/header.php
+++ src/templates/header.php
@@ -6,22 +6,29 @@
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.png" rel="shortcut icon">
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
+		<link href="https://fonts.googleapis.com/css?family=Manuale:400,500|Open+Sans" rel="stylesheet">
+
 		<?php wp_head(); ?>
-		
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+		<script src="https://use.fontawesome.com/670b7fd49d.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+
+
 		<script>
         // conditionizr.com
         // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
+        // conditionizr.config({
+        //     assets: '<?php echo get_template_directory_uri(); ?>',
+        //     tests: {}
+        // });
         </script>
 
 	</head>
@@ -33,18 +40,29 @@
 			<!-- header -->
 			<header class="header clear" role="banner">
 
-					<!-- logo -->
+					<!-- logo
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
 							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
 						</a>
 					</div>
-					<!-- /logo -->
+					/logo -->
 
 					<!-- nav -->
 					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
+						<?php
+							$menuParameters = array(
+						    'echo' => false,
+								'items_wrap'=> '%3$s',
+							  'walker' => new Nav_Footer_Walker(),
+
+							);
+
+							echo wp_nav_menu( $menuParameters );
+
+							//echo strip_tags(wp_nav_menu( $menuParameters ), '<a><nav>' );
+						?>
+
 					</nav>
 					<!-- /nav -->
 
