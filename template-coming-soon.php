65d0968d4612094ae90abc3b0dc7eed58df6dd39
Nate <natevandervis@gmail.com>
2017-10-10 22:15:46 -0500

    adding coming soon page / hardware accelerating logo animation

---
+++ template-coming-soon.php
@@ -0,0 +1,14 @@
+<?php /* Template Name: Coming Soon Template */ get_header(); ?>
+
+<main role="main">
+
+	<!-- logo -->
+	<a class="logo" href="" title=''>
+		<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Silence Kit Band Logo - Winnipeg, Canada">
+		<h4>Site launching soon...</h4>
+	</a>
+	<!-- /logo -->
+
+	<?php get_footer(); ?>
+
+</main>
